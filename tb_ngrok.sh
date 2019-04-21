9519da356f10c383e4ba1e400e0d39593dcffbbb
gnd <gnd@itchybit.org>
2019-04-21 16:19:29 +0200

    switching to bash

---
+++ tb_ngrok.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+# This opens up a tunnel to see tensorboard on google colab via ngrok
+# from on https://medium.com/deep-learning-turkey/google-colab-free-gpu-tutorial-e113627b9f5d
+
+# set the log dir
+LOG_DIR='logs'
+
+# remove old ngrok
+if [ -e ngrok-stable-linux-amd64.zip ]; then
+    rm ngrok-stable-linux-amd64.zip
+    rm ngrok
+fi
+
+# get the new ngrok, unzip & run everything
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
+unzip ngrok-stable-linux-amd64.zip
+tensorboard --logdir $LOG_DIR --host 0.0.0.0 --port 6006 &
+./ngrok http 6006 &
+curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
