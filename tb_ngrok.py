d998e0f79bd8f958e417f7f45a0e813ade2461e9
gnd <gnd@itchybit.org>
2019-04-21 15:53:07 +0200

    tensorboard tunneling

--- tb_ngrok.py
+++ tb_ngrok.py
@@ -9,10 +9,7 @@ LOG_DIR = 'logs'
 subprocess.run(['wget', 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'])
 subprocess.run(['unzip', 'ngrok-stable-linux-amd64.zip']
 
-get_ipython().system_raw(
-    'tensorboard --logdir {} --host 0.0.0.0 --port 6006 &'
-    .format(LOG_DIR))
-
+get_ipython().system_raw('tensorboard --logdir {} --host 0.0.0.0 --port 6006 &'.format(LOG_DIR))
 get_ipython().system_raw('./ngrok http 6006 &')
 
 subprocess.run(['curl', '-s', 'http://localhost:4040/api/tunnels'])
