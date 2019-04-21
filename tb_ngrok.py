7517f3a5369301ee123b90352e5f0c93a21594be
gnd <gnd@itchybit.org>
2019-04-21 15:48:31 +0200

    tensorboard tunneling

--- tb_ngrok.py
+++ tb_ngrok.py
@@ -1,15 +1,13 @@
 # This opens up a tunnel to see tensorboard on google colab via ngrok
 # from https://medium.com/deep-learning-turkey/google-colab-free-gpu-tutorial-e113627b9f5d
+import os
+import subprocess
 
 # You can change the directory name
 LOG_DIR = 'logs'
 
-!wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
-!unzip ngrok-stable-linux-amd64.zip
-
-import os
-if not os.path.exists(LOG_DIR):
-  os.makedirs(LOG_DIR)
+subprocess.run(['wget', 'https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip'])
+subprocess.run(['unzip', 'ngrok-stable-linux-amd64.zip']
 
 get_ipython().system_raw(
     'tensorboard --logdir {} --host 0.0.0.0 --port 6006 &'
@@ -17,4 +15,4 @@ get_ipython().system_raw(
 
 get_ipython().system_raw('./ngrok http 6006 &')
 
-!curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
+subprocess.run(['curl', '-s', 'http://localhost:4040/api/tunnels'])
