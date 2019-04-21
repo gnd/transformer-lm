c74efeca1517bbdae96dd194e73d0852bc5938ce
gnd <gnd@itchybit.org>
2019-04-21 16:23:08 +0200

    debugging

--- tb_ngrok.sh
+++ tb_ngrok.sh
@@ -14,6 +14,6 @@ fi
 # get the new ngrok, unzip & run everything
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
-tensorboard --logdir $LOG_DIR --host 0.0.0.0 --port 6006 &
-./ngrok http 6006 &
+get_ipython().system_raw('tensorboard --logdir {} --host 0.0.0.0 --port 6006 &'.format(LOG_DIR))
+get_ipython().system_raw('./ngrok http 6006 &')
 curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
