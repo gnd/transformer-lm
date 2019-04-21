35359a6da2012a81048566d18327934a8708c7d1
gnd <gnd@itchybit.org>
2019-04-21 16:24:32 +0200

    debugging

--- tb_ngrok.sh
+++ tb_ngrok.sh
@@ -2,9 +2,6 @@
 # This opens up a tunnel to see tensorboard on google colab via ngrok
 # from on https://medium.com/deep-learning-turkey/google-colab-free-gpu-tutorial-e113627b9f5d
 
-# set the log dir
-LOG_DIR='logs'
-
 # remove old ngrok
 if [ -e ngrok-stable-linux-amd64.zip ]; then
     rm ngrok-stable-linux-amd64.zip
@@ -14,6 +11,6 @@ fi
 # get the new ngrok, unzip & run everything
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip
-get_ipython().system_raw('tensorboard --logdir {} --host 0.0.0.0 --port 6006 &'.format(LOG_DIR))
+get_ipython().system_raw('tensorboard --logdir {logs} --host 0.0.0.0 --port 6006 &')
 get_ipython().system_raw('./ngrok http 6006 &')
 curl -s http://localhost:4040/api/tunnels | python3 -c "import sys, json; print(json.load(sys.stdin)['tunnels'][0]['public_url'])"
